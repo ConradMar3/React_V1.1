728ce11060bb046580da04c145c6b428a8ad4515
ConradMare890317 <Conradmare@gmail.com>
2021-05-06 20:11:45 +0200

    2021/05/06 (pm)
    React-Project ( My Shop {Catalog.app} )

---
+++ React_Project1/my_shop/src/Catalog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Catalog.css';
+
+class Catalog extends React.Component {
+    render() {
+        let title = "The Catalog of today " + new Date().toDateString();
+        return <div><h2>{title}</h2></div>;
+        }
+    }
+
+export default Catalog;
