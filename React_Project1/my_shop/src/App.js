53f31edff7e322706ac1599c5a613ea93757d78f
ConradMare890317 <Conradmare@gmail.com>
2021-05-09 14:40:15 +0200

    2021/05/09 (pm)

--- React_Project1/my_shop/src/App.js
+++ React_Project1/my_shop/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">The Catalog App</h1>
+          <h1 className="App-title">Catalog App</h1>
         </header>
         <Catalog />
       </div>
