de3294a417f970f709345e0bc34cfd71e3aba733
ConradMare890317 <Conradmare@gmail.com>
2021-05-09 17:56:51 +0200

    2021/05/09 (pm)

--- React_Project1/my_shop/src/Product.js
+++ React_Project1/my_shop/src/Product.js
@@ -1,10 +1,11 @@
 import React from 'react';
 class Product extends React.Component {
-render() {
-return <li>
-<h3>Product name</h3>
-<p>Product description</p>
-</li>;
+    render() {
+        return <li>
+            <h3>Product name</h3>
+                <p>Product description</p>
+        </li>;
+    }
 }
-}
+
 export default Product;
