15274e2aaa720065d88eb22f69f3459b6c015594
ConradMare890317 <Conradmare@gmail.com>
2021-05-10 19:59:23 +0200

    2021/05/10 (pm)

--- React_Project1/my_shop/src/Catalog.js
+++ React_Project1/my_shop/src/Catalog.js
@@ -3,19 +3,12 @@ import './Catalog.css';
 import ProductList from './ProductList'
 import logo from './logo.svg';
 class Catalog extends React.Component {
-    constructor() {
-        super();
-        this.state = { products: [] };
-
-        fetch("products.json")
-        .then(response => response.json())
-        .then(json => {this.setState({products: json})})
-        .catch(error => console.log(error));
-    }
-
     render() {
-        return <div><h2>Wine Catalog</h2>
-        <ProductList items={this.state.products}/>
+    let title = "The Catalog of today " + new Date().toDateString();
+
+    return <div>
+        <h2>{title}</h2>
+        <ProductList />
     
         <img src={logo} className="App-logo" alt="logo" />
         </div>;
--- React_Project1/my_shop/src/Product.js
+++ React_Project1/my_shop/src/Product.js
@@ -1,11 +1,13 @@
 import React from 'react';
 class Product extends React.Component {
-    render() {
-        return <li>
-            <h3>{this.props.item.name}</h3>
-            <p>{this.props.item.description}</p>
-        </li>;
-    }
+showPrice() {
+alert(this.props.item.price);
 }
-
+render() {
+return <li onClick={() => this.showPrice()}>
+<h3>{this.props.item.name}</h3>
+<p>{this.props.item.description}</p>
+</li>;
+}
+}
 export default Product;
