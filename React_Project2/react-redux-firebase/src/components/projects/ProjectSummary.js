cec20ec699505da16f03bafdbc73cfd32b87e49e
ConradMare890317 <Conradmare@gmail.com>
2021-05-18 22:03:02 +0200

    2021/05/18 (pm) Login form

--- React_Project2/react-redux-firebase/src/components/projects/ProjectSummary.js
+++ React_Project2/react-redux-firebase/src/components/projects/ProjectSummary.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
 const ProjectSummary = () => {
-    return(
+    return (
 
         <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-            <span className="card-title">React/Redux - Projects Lists</span>
-            <p>Posted by Conrad Maré</p>
-            <p className="grey-text">2021/05/18</p>
+            <div className="card-content grey-text text-darken-3">
+                <span className="card-title">React/Redux/Firebase - Projects</span>
+                <p>Posted by Conrad Maré</p>
+                <p className="grey-text">2021/05/18</p>
+            </div>
         </div>
-    </div>
 
     )
 }
