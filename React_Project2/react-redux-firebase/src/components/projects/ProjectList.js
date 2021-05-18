de2747a8d365290b8378935f0ddb76207e01c707
ConradMare890317 <Conradmare@gmail.com>
2021-05-18 16:43:37 +0200

    2021/05/18 (pm)

--- React_Project2/react-redux-firebase/src/components/projects/ProjectList.js
+++ React_Project2/react-redux-firebase/src/components/projects/ProjectList.js
@@ -1,33 +1,13 @@
 import React from 'react'
+import ProjectSummary from './ProjectSummary'
 
 const ProjectList = () => {
     return (
         <div className="project-list section">
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">React/Redux - DashBoard</span>
-                    <p>Posted by Conrad Maré</p>
-                    <p className="grey-text">2021/05/18 11:50am</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Conrad Maré</p>
-                    <p className="grey-text">2021/05/18</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Conrad Maré</p>
-                    <p className="grey-text">2021/05/18</p>
-                </div>
-            </div>
-
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
         </div>
     )
 }
