7c119b20f07104bd945aab92911ca617f861682f
ConradMare890317 <Conradmare@gmail.com>
2021-05-20 12:41:36 +0200

    2021/05/20 (pm)

--- React_Project1/react-login-service/src/index.js
+++ React_Project1/react-login-service/src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import serviceWorker from './registerServiceWorker';
-import firebase from '@firebase/app';
-import 'firebase/firestore';
+import * as firebase from 'firebase';
 
 
 var config = {
