75a968daa9f62f48545c996042d6a956688a72a3
ConradMare890317 <Conradmare@gmail.com>
2021-05-25 19:38:29 +0200

    2021/05/25 (pm) Firebase config

--- React_Project2/react-redux-firebase/src/config/fbConfig.js
+++ React_Project2/react-redux-firebase/src/config/fbConfig.js
@@ -1,20 +1,22 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
-import 'firebase/database'
+import 'firebase/auth'
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-    apiKey: "AIzaSyDndchZsYMM24kMRK8IrMYrIRn8nsgMx70",
-    authDomain: "react-redux-firebase-9698b.firebaseapp.com",
-    projectId: "react-redux-firebase-9698b",
-    storageBucket: "react-redux-firebase-9698b.appspot.com",
-    messagingSenderId: "553235523641",
-    appId: "1:553235523641:web:50706fa71078819258c5e8",
-    measurementId: "G-QP03MD71ZX"
+const firebaseConfig = {
+  apiKey: "AIzaSyDndchZsYMM24kMRK8IrMYrIRn8nsgMx70",
+  authDomain: "react-redux-firebase-9698b.firebaseapp.com",
+  databaseURL: "https://react-redux-firebase-9698b-default-rtdb.firebaseio.com",
+  projectId: "react-redux-firebase-9698b",
+  storageBucket: "react-redux-firebase-9698b.appspot.com",
+  messagingSenderId: "553235523641",
+  appId: "1:553235523641:web:c9fb683dc02ce18758c5e8",
+  measurementId: "G-RGC2XQDSRF"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
 firebase.analytics();
 
 export default firebaseConfig;
